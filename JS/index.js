a2d9a959be20867715eabab43f4fce4e67c9928d
Juan Francisco <contrerasjf.0@hotmail.com>
2020-04-10 17:48:41 -0500

    Implement the classes kind of Cars servers in Python and Javascript
    * The  UberBlac, UberPool, UberVan and UberX were implemented in Python and Javascript.
    
    Resolve C-27

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,12 @@
 const Car = require('./Car');
 const Account = require('./Account');
+const UberX = require('./UberX');
 
 const car = new Car('WEW323', new Account('Marcos Luis', 'Y4U23'));
 car.passanger = 4;
 car.printDataCar();
+
+const uberX = new UberX('HSAAW2', new Account('Luis Gonzales', 'A23DW'), "Kia", "Rio");
+
+uberX.passanger = 3;
+uberX.printDataCar();
